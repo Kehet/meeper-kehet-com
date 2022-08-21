0a18b284ae54997a7a11d1e3e550c35b8a1641ee
Kehet <382965-kehet@users.noreply.gitlab.com>
2022-08-21 15:08:37 +0300

    force https feature

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // because of reverse proxy
+        if (config('app.force_https')) {
+            URL::forceScheme('https');
+        }
     }
 }
