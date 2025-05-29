37ffeba01027f531063f28a019850cf0fbb7500b
Kehet <Kehet@users.noreply.github.com>
2025-05-29 20:24:55 +0300

    Set up CI and production workflows with Docker integration

---
+++ docker/Dockerfile.php
@@ -0,0 +1,32 @@
+############################################
+# Base Image
+############################################
+
+# Learn more about the Server Side Up PHP Docker Images at:
+# https://serversideup.net/open-source/docker-php/
+FROM serversideup/php:8.3-fpm-nginx-alpine AS base
+
+## Uncomment if you need to install additional PHP extensions
+USER root
+RUN install-php-extensions gd imagick
+
+############################################
+# CI image
+############################################
+FROM base AS ci
+
+# Sometimes CI images need to run as root
+# so we set the ROOT user and configure
+# the PHP-FPM pool to run as www-data
+USER root
+RUN echo "" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf && \
+    echo "user = www-data" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf && \
+    echo "group = www-data" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf
+
+############################################
+# Production Image
+############################################
+FROM base AS deploy
+COPY --chown=www-data:www-data . /var/www/html
+
+USER www-data
